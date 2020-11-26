2f923e6bc78c69ba5e208f975a9db5722de48845
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-26 23:29:14 +0100

    FEAT: Added index and show action for teachers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'teachers/index'
+  get 'teachers/show'
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :teachers, only: [:index, :show]
 end
